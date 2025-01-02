1960ee79b324fe21af96054028df312a7649b046
Karl Du <48907733+CarlDuFromChina@users.noreply.github.com>
2025-01-02 16:04:00 +0800

    更新marked库至2.1.3，优化postReadonly组件的内容渲染，调整样式以改善Markdown显示效果

--- Sixpence.UI/vue-mobile-public/src/module/post/postReadonly.vue
+++ Sixpence.UI/vue-mobile-public/src/module/post/postReadonly.vue
@@ -2,7 +2,7 @@
   <sp-view>
     <sp-header :title="data.title"></sp-header>
     <sp-content>
-      <div v-html="content" class="content markdown-body"></div>
+      <article v-html="content" class="content markdown-body"></article>
     </sp-content>
   </sp-view>
 </template>
@@ -28,9 +28,8 @@ export default {
       sp.get(`api/post/${this.Id}`)
         .then(resp => {
           this.data = resp;
-          this.content = marked(resp.content, {
-            sanitize: true
-          });
+          this.content = marked(resp.content);
+          console.log(this.content);
         })
         .finally(() => {
           setTimeout(() => {
@@ -55,5 +54,61 @@ h2 {
 /deep/ .markdown-body img {
   max-width: 100%;
   height: auto;
+}
+
+/deep/ .markdown-body {
+  line-height: 1.5;
+  font-size: 14px;
+  color: #333;
+  word-wrap: break-word;
+}
+
+/deep/ .markdown-body h1,
+/deep/ .markdown-body h2,
+/deep/ .markdown-body h3,
+/deep/ .markdown-body h4,
+/deep/ .markdown-body h5,
+/deep/ .markdown-body h6 {
+  margin-top: 1em;
+  margin-bottom: 0.5em;
+  font-weight: bold;
+  border-bottom: 1px solid #eaecef;
+}
+
+/deep/ .markdown-body a {
+  color: #0366d6;
+  text-decoration: none;
+}
+
+/deep/.markdown-body code {
+  background-color: #f6f8fa;
+  padding: 2px 4px;
+  border-radius: 3px;
+}
+
+/deep/ .markdown-body table {
+  border-collapse: collapse;
+  width: 100%;
+  margin-bottom: 1em;
+}
+
+/deep/ .markdown-body th,
+/deep/ .markdown-body td {
+  border: 1px solid #ddd;
+  padding: 0.5em;
+}
+
+/deep/ .markdown-body th {
+  background-color: #f6f8fa;
+}
+
+/deep/ .markdown-body img {
+  max-width: 100%;
+  height: auto;
+}
+
+/deep/ .markdown-body iframe {
+  width: 100%;
+  height: 30vh;
 }
 </style>
