e2dfe818ae64c1f3eee48a9ce9f6a91daf74eaf6
Karl Du <48907733+CarlDuFromChina@users.noreply.github.com>
2025-04-09 14:50:06 +0800

    缓存接口添加注释

--- Sixpence.Core/Sixpence.Common/Cache/ICacheService.cs
+++ Sixpence.Core/Sixpence.Common/Cache/ICacheService.cs
@@ -6,14 +6,42 @@ using System.Threading.Tasks;
 
 namespace Sixpence.Common.Cache
 {
+    /// <summary>
+    /// 缓存服务接口
+    /// </summary>
     public interface ICacheService
     {
+        /// <summary>
+        /// 获取或创建缓存
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="dataGetter"></param>
+        /// <param name="expiry"></param>
+        /// <returns></returns>
         Task<T> GetOrCreateAsync<T>(string key, Func<Task<T>> dataGetter, TimeSpan? expiry = null);
 
+        /// <summary>
+        /// 获取或创建缓存
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="dataGetter"></param>
+        /// <param name="expiry"></param>
+        /// <returns></returns>
         T GetOrCreate<T>(string key, Func<T> dataGetter, TimeSpan? expiry = null);
 
+        /// <summary>
+        /// 移除缓存
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         Task RemoveAsync(string key);
 
+        /// <summary>
+        /// 移除缓存
+        /// </summary>
+        /// <param name="key"></param>
         void Remove(string key);
     }
 }
