01f23762e851dff5d7dbd061c034945a7bbd4e3c
Karl Du <18556906294@163.com>
2025-08-10 13:10:52 +0800

    更新依赖库的 CDN 链接

--- Sixpence.UI/vue-pc-public/vite.config.js
+++ Sixpence.UI/vue-pc-public/vite.config.js
@@ -19,33 +19,33 @@ export default defineConfig(( mode ) => {
           {
             name: 'vue',
             var: 'Vue',
-            path: 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/vue/2.6.14/vue.min.js'
+            path: 'https://cdn.bootcdn.net/ajax/libs/vue/2.6.14/vue.min.js'
           },
           {
             name: 'vue-router',
             var: 'VueRouter',
-            path: 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/vue-router/3.5.3/vue-router.min.js'
+            path: 'https://cdn.bootcdn.net/ajax/libs/vue-router/3.5.3/vue-router.min.js'
           },
           {
             name: 'vuex',
             var: 'Vuex',
-            path: 'https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/vuex/3.6.2/vuex.min.js'
+            path: 'https://cdn.bootcdn.net/ajax/libs/vuex/3.6.2/vuex.min.js'
           },
           {
             name: 'moment',
             var: 'moment',
-            path: 'https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/moment.js/2.29.1/moment.min.js'
+            path: 'https://cdn.bootcdn.net/ajax/libs/moment.js/2.29.1/moment.min.js'
           },
           {
             name: 'ant-design-vue',
             var: 'antd',
-            path: 'https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/ant-design-vue/1.7.8/antd.min.js',
-            css: 'https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/ant-design-vue/1.7.8/antd.min.css'
+            path: 'https://cdn.bootcdn.net/ajax/libs/ant-design-vue/1.7.8/antd.min.js',
+            css: 'https://cdn.bootcdn.net/ajax/libs/ant-design-vue/1.7.8/antd.min.css'
           },
           {
             name: 'marked',
             var: 'marked',
-            path: 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/marked/2.1.3/marked.min.js'
+            path: 'https://cdn.bootcdn.net/ajax/libs/marked/2.1.3/marked.min.js'
           }
         ]
       }),
